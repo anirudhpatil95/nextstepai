b5c6e0e926a27494621d477da14aa9c7f97abfc4
Anirudh Patil <121297107+anirudhpatil95@users.noreply.github.com>
2025-05-18 11:17:36 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Supabase Configuration
 const SUPABASE_CONFIG = {
     url: 'https://gdvnhzibynjanakeofef.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2bm5oeWlieW5qYW5ha2VvZmVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU5MjM5MjMsImV4cCI6MjAyMTUwOTkyM30.00000000000000000000000000000000000000000000000000'
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdkdm5oemlieW5qYW5ha2VvZmVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0ODYzNjMsImV4cCI6MjA2MzA2MjM2M30.ruppN5sB-IK-Ch-o_8FR7Tb4rSt0-h3sygV_vpMmjyo'
 }
 
 export { SUPABASE_CONFIG } 
